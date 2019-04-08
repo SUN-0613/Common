52a150ee201c382b23e7cc781393f763cc24f7ab
AkihiroYamamoto <akiyam0613@gmail.com>
2019-04-08 11:26:22 +0900

    Controls用Common作成

---
+++ CommonControls/Custom/TreeView.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows;
+
+namespace AYam.Common.Controls.Custom
+{
+
+    /// <summary>
+    /// TreeViewカスタマイズ
+    /// </summary>
+    /// <remarks>
+    /// SelectedItemをBinding可
+    /// </remarks>
+    public class TreeView : System.Windows.Controls.TreeView, IDisposable
+    {
+
+        #region DependencyProperty
+
+        /// <summary>
+        /// SelectedItem依存プロパティ
+        /// </summary>
+        public static readonly DependencyProperty BindableSelectedItemProperty
+                                        = DependencyProperty.Register(
+                                        nameof(BindableSelectedItem)
+                                        , typeof(object)
+                                        , typeof(AYam.Common.Controls.Custom.TreeView)
+                                        , new UIPropertyMetadata(null));
+
+        #endregion
+
+        #region Property
+
+        /// <summary>
+        /// SelectedItemプロパティ
+        /// </summary>
+        public object BindableSelectedItem
+        {
+            get { return GetValue(BindableSelectedItemProperty); }
+            set { SetValue(BindableSelectedItemProperty, value); }
+        }
+
+        #endregion
+
+        #region Event
+
+        /// <summary>
+        /// 選択箇所変更イベント
+        /// </summary>
+        protected virtual void OnSelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+
+            if (SelectedItem != null)
+            {
+                SetValue(BindableSelectedItemProperty, SelectedItem);
+            }
+
+        }
+
+        #endregion
+
+        /// <summary>
+        /// TreeViewカスタマイズ
+        /// </summary>
+        public TreeView()
+        {
+            SelectedItemChanged += OnSelectedItemChanged;
+        }
+
+        /// <summary>
+        /// 終了処理
+        /// </summary>
+        public void Dispose()
+        {
+            SelectedItemChanged -= OnSelectedItemChanged;
+        }
+
+    }
+
+}
