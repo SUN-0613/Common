1bfe8d30ee06b5f67e5c7805bd49d343d7b9da79
AkihiroYamamoto <akiyam0613@gmail.com>
2019-05-17 11:53:49 +0900

    Window.Closing用ビヘイビア作成

---
+++ CommonControls/Behaviors/WindowClosing.cs
@@ -0,0 +1,54 @@
+﻿using AYam.Common.Controls.Interface;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Interactivity;
+
+namespace AYam.Common.Controls.Behaviors
+{
+
+    /// <summary>
+    /// Window.Closing用ビヘイビア
+    /// </summary>
+    public class WindowClosing : Behavior<Window>
+    {
+
+        /// <summary>
+        /// ビヘイビア登録
+        /// </summary>
+        protected override void OnAttached()
+        {
+
+            base.OnAttached();
+
+            AssociatedObject.Closing += OnClosing;
+
+        }
+
+        /// <summary>
+        /// ビヘイビア解除
+        /// </summary>
+        protected override void OnDetaching()
+        {
+
+            base.OnDetaching();
+
+            AssociatedObject.Closing -= OnClosing;
+
+        }
+
+        /// <summary>
+        /// Window.Closingイベント
+        /// </summary>
+        private void OnClosing(object sender, CancelEventArgs e)
+        {
+
+            if (sender is Window form && form.DataContext is IClosing viewModel)
+            {
+                e.Cancel = viewModel.OnClosing();
+            }
+
+        }
+
+    }
+
+}
---
+++ CommonControls/Interface/IClosing.cs
@@ -0,0 +1,18 @@
+﻿namespace AYam.Common.Controls.Interface
+{
+
+    /// <summary>
+    /// Window.Closing用Interface
+    /// </summary>
+    public interface IClosing
+    {
+
+        /// <summary>
+        /// Window.Closing処理実装
+        /// </summary>
+        /// <returns></returns>
+        bool OnClosing();
+
+    }
+
+}
