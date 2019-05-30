7e549f8d38828e5a30cf4ad13e84406c98da81cd
AkihiroYamamoto <akiyam0613@gmail.com>
2019-05-30 11:21:29 +0900

    データ管理用クラス、Dispose実装List、データ変換管理クラスの作成

---
+++ Common/Method/ValueConvert.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+
+namespace AYam.Common.Method
+{
+
+    /// <summary>
+    /// 値変換管理共通メソッド
+    /// </summary>
+    public static class ValueConvert
+    {
+
+        /// <summary>
+        /// 文字列を型Tに変換
+        /// </summary>
+        /// <typeparam name="T">型指定</typeparam>
+        /// <param name="value">値</param>
+        /// <returns>変換値</returns>
+        public static T FromString<T>(string value)
+        {
+
+            var conv = TypeDescriptor.GetConverter(typeof(T));
+
+            if (conv != null)
+            {
+                return (T)conv.ConvertFromString(value);
+            }
+            else
+            {
+                return default(T);
+            }
+
+        }
+
+    }
+}
