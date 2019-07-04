4d11c2c1fd91cc5d6ab38725658cbf9ba7028e35
AkihiroYamamoto <akiyam0613@gmail.com>
2019-07-04 17:27:44 +0900

    TextBox.TextProperty更新用トリガのビヘイビア追加

---
+++ CommonControls/TriggerBehaviors/TextBoxUpdate.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Interactivity;
+
+namespace AYam.Common.Controls.TriggerBehaviors
+{
+
+    /// <summary>
+    /// TextBox用ビヘイビア
+    /// イベント発生時にTextPropertyを更新するトリガ
+    /// </summary>
+    public class TextBoxUpdate : TriggerAction<TextBox>
+    {
+
+        /// <summary>
+        /// TextProperty.Binding
+        /// </summary>
+        private BindingExpression _Binding;
+
+        /// <summary>
+        /// ビヘイビア登録
+        /// </summary>
+        protected override void OnAttached()
+        {
+
+            base.OnAttached();
+            _Binding = AssociatedObject.GetBindingExpression(TextBox.TextProperty);
+
+        }
+
+        /// <summary>
+        /// ビヘイビア解除
+        /// </summary>
+        protected override void OnDetaching()
+        {
+
+            _Binding = null;
+            base.OnDetaching();
+
+        }
+
+        /// <summary>
+        /// TextProperty更新
+        /// </summary>
+        /// <param name="parameter"></param>
+        protected override void Invoke(object parameter)
+        {
+            
+            if (_Binding != null)
+            {
+                _Binding.UpdateSource();
+            }
+
+        }
+
+    }
+}
