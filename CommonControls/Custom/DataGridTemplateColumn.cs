2bbd00e87093d698fbf985d3a278033d93fea53c
AkihiroYamamoto <akiyam0613@gmail.com>
2019-08-29 16:47:00 +0900

    DataGridコントロールの追加

---
+++ CommonControls/Custom/DataGridTemplateColumn.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System.Windows;
+using System.Windows.Input;
+
+namespace AYam.Common.Controls.Custom
+{
+
+    /// <summary>
+    /// DataGridTemplateColumnカスタマイズ
+    /// </summary>
+    /// <remarks>
+    /// 内部コントロールにフォーカスを当てる
+    /// </remarks>
+    public class DataGridTemplateColumn : System.Windows.Controls.DataGridTemplateColumn
+    {
+
+        /// <summary>
+        /// 列内のセルを編集モードに変更
+        /// </summary>
+        /// <param name="editingElement">列内に表示される要素</param>
+        /// <param name="editingEventArgs">ユーザジェスチャ情報</param>
+        /// <returns>セル編集前の値</returns>
+        protected override object PrepareCellForEdit(FrameworkElement editingElement, RoutedEventArgs editingEventArgs)
+        {
+
+            // セル内部コントロールへフォーカス移動
+            editingElement.MoveFocus(new TraversalRequest(FocusNavigationDirection.First));
+
+            return base.PrepareCellForEdit(editingElement, editingEventArgs);
+
+        }
+
+    }
+
+}
---
+++ CommonControls/ResourceDictionary/DataGridSingleClickStyle.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace AYam.Common.Controls.ResourceDictionary
+{
+    partial class DataGridSingleClickStyle : System.Windows.ResourceDictionary
+    {
+
+        /// <summary>
+        /// DataGridCell.Single Click Editing
+        /// </summary>
+        private void DataGridCell_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+            if (sender is DataGridCell cell)
+            {
+
+                // 編集中のセル、読取専用のセルは除外
+                if (!cell.IsEditing && !cell.IsReadOnly)
+                {
+
+                    // 未フォーカスならフォーカスセット
+                    if (!cell.IsFocused)
+                    {
+                        cell.Focus();
+                    }
+
+                    // 対象セルを保有するDataGridを取得
+                    var dataGrid = FindVisualParent<DataGrid>(cell);
+
+                    if (dataGrid != default(DataGrid))
+                    {
+
+                        // 行単位で選択する設定となっているか
+                        if (dataGrid.SelectionUnit != DataGridSelectionUnit.FullRow)
+                        {
+
+                            // セル単位で選択
+                            if (!cell.IsSelected)
+                            {
+                                cell.IsSelected = true;
+                            }
+
+                        }
+                        else
+                        {
+
+                            // 対象セルを保有するDataGridRowを取得
+                            var row = FindVisualParent<DataGridRow>(cell);
+
+                            // 行単位で選択
+                            if (row != default(DataGridRow) && !row.IsSelected)
+                            {
+                                row.IsSelected = true;
+                            }
+
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        /// <summary>
+        /// 指定要素より上位の部分で指定型の要素を検索
+        /// </summary>
+        /// <typeparam name="T">指定型</typeparam>
+        /// <param name="element">指定要素</param>
+        /// <returns>指定型の要素、取得できない場合はdefault値</returns>
+        private static T FindVisualParent<T>(UIElement element)
+        {
+
+            var parent = element;
+
+            while (parent != null)
+            {
+
+                if (parent is T correctlyTyped)
+                {
+                    return correctlyTyped;
+                }
+
+                parent = VisualTreeHelper.GetParent(parent) as UIElement;
+
+            }
+
+            return default;
+
+        }
+
+    }
+
+}
